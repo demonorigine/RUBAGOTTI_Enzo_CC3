63a17869fec22624a6c268e7ac80caa68e7883df
demonorigine <enzo.rubagotti@outlook.com>
2024-09-25 18:29:45 +1100

    Ajout de view/random.ejs

--- devweb-tp5/server-express.mjs
+++ devweb-tp5/server-express.mjs
@@ -9,16 +9,24 @@ const app = express();
 //middleware pour trouver le chemin des fichiers static
 app.use(express.static("static"));
 
+//moteur de rendu par défaut
+app.set("view engine", "ejs");
+
+
 app.get(["/", "/index.html"], async function (request, response, next) {
   response.sendFile("index.html", { root: "./" });
 });
 
 app.get("/random/:nb", async function (request, response, next) {
-  const length = request.params.nb;
-  const contents = Array.from({ length })
-    .map((_) => `<li>${Math.floor(100 * Math.random())}</li>`)
-    .join("\n");
-  return response.send(`<html><ul>${contents}</ul></html>`);
+  const length = parseInt(request.params.nb, 10);
+
+  // Générer un tableau de nombres aléatoires
+  const numbers = Array.from({ length }).map(() => Math.floor(100 * Math.random()));
+
+  // Message de bienvenue
+  const welcome = `Voici ${length} nombres aléatoires`;
+
+  response.render("random", { numbers, welcome });
 });
 
 const server = app.listen(port, host);
